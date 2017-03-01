f7212e341190b282d0ba7dc56eab31d7c4d7491e
jbrys <jeffreybrys@gmail.com>
2017-02-28 17:56:56 -0800

    inital commit of timeline

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import About from './about';
+import Timeline from './timeline';
 
 export default class IndexPage extends React.Component {
   render() {
     return (
       <div className="home">
         <About />
-        <About />
+        <Timeline />
         <About />
       </div>
     );
